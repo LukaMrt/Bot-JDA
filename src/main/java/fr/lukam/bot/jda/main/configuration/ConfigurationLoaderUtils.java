555a4015484ce10e51c1a415fa906d38e1370f57
LukaMrt <maret.luka@gmail.com>
2020-03-05 21:53:13 +0100

    🚑 fix configuration type bug

--- src/main/java/fr/lukam/bot/jda/main/configuration/ConfigurationLoaderUtils.java
+++ src/main/java/fr/lukam/bot/jda/main/configuration/ConfigurationLoaderUtils.java
@@ -3,6 +3,8 @@ package fr.lukam.bot.jda.main.configuration;
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import com.electronwill.nightconfig.core.file.FileConfig;
 
+import java.util.ArrayList;
+
 public class ConfigurationLoaderUtils {
 
     public static Configuration loadFrom(String filePath) {
@@ -18,7 +20,7 @@ public class ConfigurationLoaderUtils {
                 .withToken(fileConfig.getOrElse("bot.token", ""))
                 .withPrefix(fileConfig.getCharOrElse("bot.prefix", ' '))
                 .withOwnerId(fileConfig.getOrElse("ids.owner_id", ""))
-                .withCoOwnerIds(fileConfig.getOrElse("ids.co_owners_ids", new String[]{}))
+                .withCoOwnerIds(fileConfig.getOrElse("ids.co_owners_ids", new ArrayList<String>()).toArray(new String[]{}))
                 .withMainServerId(fileConfig.getOrElse("ids.main_server_id", ""))
                 .build();
 
